b436e44d7dcc4772fe2953b1410f1a3ee3577e9b
A S Rakesh Krishna <rakeshkrishna517@gmail.com>
2021-03-24 15:24:39 +0530

    Update tables
    formatted the code

--- tables.sql
+++ tables.sql
@@ -1,1 +1,12 @@
-CREATE TABLE `youtubetask`. ( `videoId` VARCHAR(12) NOT NULL , `title` VARCHAR(100) NOT NULL , `description` VARCHAR(500) NOT NULL , `publishedAt` DATETIME NOT NULL , `channelId` VARCHAR(50) NOT NULL , `defaultThumbnail` VARCHAR(50) NOT NULL , `mediumThumbnail` VARCHAR(50) NOT NULL , `highThumbnail` VARCHAR(50) NOT NULL , PRIMARY KEY (`videoId`)) ENGINE = InnoDB;
+CREATE TABLE `youtubetask`. 
+(   
+    `videoId` VARCHAR(12) NOT NULL , 
+    `title` VARCHAR(100) NOT NULL ,
+    `description` VARCHAR(500) NOT NULL ,
+    `publishedAt` DATETIME NOT NULL ,
+    `channelId` VARCHAR(50) NOT NULL ,
+    `defaultThumbnail` VARCHAR(50) NOT NULL ,
+    `mediumThumbnail` VARCHAR(50) NOT NULL ,
+    `highThumbnail` VARCHAR(50) NOT NULL ,
+     PRIMARY KEY (`videoId`)
+     ) ENGINE = InnoDB;
